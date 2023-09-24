d1d6a1eda52a70073dbf376775f0492fde7711a6
Pruthviraj <pruthviraj9mane@gmail.com>
2023-09-24 12:38:03 +0530

    making dice game

--- lib/FirstCode.dart
+++ lib/FirstCode.dart
@@ -4,12 +4,29 @@ class FirstCode extends StatelessWidget{
    FirstCode(this.text,{super.key});
 
   final String text;
+
+  void rollDice(){
+
+  }
+
   @override
   Widget build(context){
     return  Scaffold(
       body: Center(
         child: Center(
-            child: Text(text)
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assests/images/dice-2.png',
+                    width: 100,
+                    height: 100
+                ),
+                TextButton(onPressed: rollDice,
+                    child: const  Text('Roll Dice'),
+                )
+
+              ],
+            )
         ),
       ),
     );
